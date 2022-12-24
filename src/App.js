ac03efedc1fd7a89bdd6fdbf64761b76832d8545
A350 <85403534+Lucif3r-in@users.noreply.github.com>
2022-12-24 22:14:02 +0530

    fix:merge conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-
-import Surgery from "./components/Surgery";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Surgery/>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
